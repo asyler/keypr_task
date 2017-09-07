e3816a8f47f0b73eb0bcc52dbed917051a5aff7c
asyler <vladomar@gmail.com>
2017-09-07 17:18:46 +0300

    More model failing tests

--- reservations/tests/test_models.py
+++ reservations/tests/test_models.py
@@ -4,22 +4,24 @@ from django.utils import timezone
 
 from reservations.models import Reservation
 
+
 class ReservationModelTest(TestCase):
-    def test_start_date_cannot_be_empty(self):
+    def _test_field_cannot_be_empty(self, field_name):
         reservation = Reservation()
         with self.assertRaises(ValidationError):
-            Reservation._meta.get_field('start_date').clean(reservation.start_date, reservation)
+            Reservation._meta.get_field(field_name).clean(getattr(reservation, field_name), reservation)
 
-    def test_end_date_cannot_be_empty(self):
-        reservation = Reservation()
-        with self.assertRaises(ValidationError):
-            Reservation._meta.get_field('end_date').clean(reservation.end_date, reservation)
-
+    def test_fields_cannot_be_empty(self):
+        self._test_field_cannot_be_empty('start_date')
+        self._test_field_cannot_be_empty('end_date')
+        self._test_field_cannot_be_empty('first_name')
+        self._test_field_cannot_be_empty('last_name')
+        self._test_field_cannot_be_empty('room')
 
     def test_start_date_should_be_before_end_date(self):
         reservation = Reservation(
-            start_date = timezone.now(),
-            end_date = timezone.now()-timezone.timedelta(minutes=1)
+            start_date=timezone.now(),
+            end_date=timezone.now() - timezone.timedelta(minutes=1)
         )
         with self.assertRaises(ValidationError):
             reservation.save()
