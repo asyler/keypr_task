7f4e56664204ff108903f64f5f6089d2acb615b5
asyler <vladomar@gmail.com>
2017-09-07 17:15:02 +0300

    Initial model for reservation

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+def validate_end_datetime(value):
+    pass
+
 # Create your models here.
+class Reservation(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(validators=[validate_end_datetime])
+
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError('End date should be after start date')
--- reservations/tests/test_models.py
+++ reservations/tests/test_models.py
@@ -5,11 +5,16 @@ from django.utils import timezone
 from reservations.models import Reservation
 
 class ReservationModelTest(TestCase):
-    def test_cannot_save_with_any_empty_field(self):
+    def test_start_date_cannot_be_empty(self):
         reservation = Reservation()
         with self.assertRaises(ValidationError):
-            reservation.save()
-            reservation.full_clean()
+            Reservation._meta.get_field('start_date').clean(reservation.start_date, reservation)
+
+    def test_end_date_cannot_be_empty(self):
+        reservation = Reservation()
+        with self.assertRaises(ValidationError):
+            Reservation._meta.get_field('end_date').clean(reservation.end_date, reservation)
+
 
     def test_start_date_should_be_before_end_date(self):
         reservation = Reservation(
